29ea2c6d66d3ad0ab11935ef50929b6eff0eaa80
noel <noelantoine974@outlook.fr>
2022-08-27 07:55:37 +0400

    server

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+require ("dotenv").config()
+
+
+/*** importer helmet pour sécuriser HTTP headers ***/
+
+const helmet = require("helmet");
+
+/*** importer le module express-rate-limit pour limiter le nombre de requêtes que peut faire un utilisateur ***/
+
+const rateLimit = require("express-rate-limit");
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000,
+    /*** pour chaque 10 minutes ***/
+    max: 40 /*** L'utilisateur pourra faire 40 requêtes toutes les 10 minutes ***/
+});
+const app = express(); /*** appeler express pour créer notre application express ***/
+
+
+app.listen(3000, (req, res)=> {
+    console.log('"Server connexion...')
+})
