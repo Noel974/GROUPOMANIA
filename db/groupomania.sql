ba6950b55501f0af074232eef9ae051f4237e9d8
noel <noelantoine974@outlook.fr>
2022-10-04 12:05:09 +0400

    bd

--- db/groupomania.sql
+++ db/groupomania.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 13 sep. 2022 à 19:07
+-- Généré le : mar. 04 oct. 2022 à 10:02
 -- Version du serveur : 10.4.24-MariaDB
 -- Version de PHP : 8.1.6
 
@@ -26,32 +26,147 @@ SET time_zone = "+00:00";
 --
 -- Structure de la table `comments`
 --
--- Erreur de lecture de structure pour la table groupomania.comments : #1932 - Table &#039;groupomania.comments&#039; doesn&#039;t exist in engine
--- Erreur de lecture des données pour la table groupomania.comments : #1064 - Erreur de syntaxe près de &#039;FROM `groupomania`.`comments`&#039; à la ligne 1
+
+CREATE TABLE `comments` (
+  `id` smallint(5) UNSIGNED NOT NULL,
+  `userId` smallint(5) UNSIGNED NOT NULL,
+  `postId` smallint(5) UNSIGNED NOT NULL,
+  `content` text NOT NULL,
+  `imageUrl` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `posts`
 --
--- Erreur de lecture de structure pour la table groupomania.posts : #1932 - Table &#039;groupomania.posts&#039; doesn&#039;t exist in engine
--- Erreur de lecture des données pour la table groupomania.posts : #1064 - Erreur de syntaxe près de &#039;FROM `groupomania`.`posts`&#039; à la ligne 1
+
+CREATE TABLE `posts` (
+  `id` smallint(5) UNSIGNED NOT NULL,
+  `userId` smallint(5) UNSIGNED NOT NULL,
+  `content` text NOT NULL,
+  `imageUrl` varchar(255) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `sequelizemeta`
 --
--- Erreur de lecture de structure pour la table groupomania.sequelizemeta : #1932 - Table &#039;groupomania.sequelizemeta&#039; doesn&#039;t exist in engine
--- Erreur de lecture des données pour la table groupomania.sequelizemeta : #1064 - Erreur de syntaxe près de &#039;FROM `groupomania`.`sequelizemeta`&#039; à la ligne 1
+
+CREATE TABLE `sequelizemeta` (
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `sequelizemeta`
+--
+
+INSERT INTO `sequelizemeta` (`name`) VALUES
+('20220926175539-create-user.js'),
+('20220926180313-create-comment.js'),
+('20220926180328-create-post.js');
 
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `users`
 --
--- Erreur de lecture de structure pour la table groupomania.users : #1932 - Table &#039;groupomania.users&#039; doesn&#039;t exist in engine
--- Erreur de lecture des données pour la table groupomania.users : #1064 - Erreur de syntaxe près de &#039;FROM `groupomania`.`users`&#039; à la ligne 1
+
+CREATE TABLE `users` (
+  `id` smallint(5) UNSIGNED NOT NULL,
+  `imageUrl` varchar(255) DEFAULT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `isAdmin` tinyint(4) NOT NULL DEFAULT 0,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `imageUrl`, `firstName`, `lastName`, `email`, `password`, `isAdmin`, `createdAt`, `updatedAt`) VALUES
+(4, NULL, 'test', 'set', 'test@set.fr', 'Testset974', 0, NULL, NULL),
+(5, NULL, '', 'dmina', 'admin@dmina.fr', '$2b$10$taD0JEb3bkUrEmcOaaMFNOT8CyN1xUepirjUgPp4xSxUgbpI.ejWu', 0, '2022-09-27 07:44:06', '2022-09-27 07:44:06'),
+(7, NULL, '', 'piro', 'lort@piro.fr', '$2b$10$LarJCIzE//agZ0N9kT9M5O5Uuo3PmyAq9SR6VP/fvKYrf5ZF3Ndga', 0, '2022-10-04 07:55:09', '2022-10-04 07:55:09');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_comment_post_id` (`postId`),
+  ADD KEY `fk_comment_user_id` (`userId`);
+
+--
+-- Index pour la table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_post_userId` (`userId`);
+
+--
+-- Index pour la table `sequelizemeta`
+--
+ALTER TABLE `sequelizemeta`
+  ADD PRIMARY KEY (`name`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT pour la table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `fk_comment_post_id` FOREIGN KEY (`postId`) REFERENCES `posts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_comment_user_id` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `fk_post_userId` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
